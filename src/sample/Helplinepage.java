9ef617996b36b12baa90bc0e256453648e1a20c3
anjali <dwivedianjali10@gmail.com>
2020-10-26 19:32:03 +0530

    totalcase displayed..

--- src/sample/Helplinepage.java
+++ src/sample/Helplinepage.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import models.advisoryapi;
